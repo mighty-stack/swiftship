8ee7dda77ace4b78c38d3c0e2f99ff0eed80867d
Your Name <alabiolumide@gmail.com>
2026-01-14 12:23:47 +0100

    2nd commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import Store from './Store/Store';
-import './styles/main.css';
+import './Styles/main.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
